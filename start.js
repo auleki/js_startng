fa387636898d8ba5900e6279d8e514f696960854
auleki <kodagiwa@gmail.com>
2020-04-19 22:07:04 +0100

    Task completed

--- start.js
+++ start.js
@@ -3,20 +3,25 @@ const checkNum = (num) => {
     for(z = 1; z <= num; z++) {
         allNumbers.push(z)
     }
-    // console.log(allNumbers)
     allNumbers.forEach((num, index) => {
-        // console.log(`Number ${num} :::: Index ${index}`);
-        if(num % 2 === 0) {
-            allNumbers[index] = 'yi';
-            // console.log(`Number ${num} :::: Index ${index}`);            
+        if(num % 2 === 0 && num % 3 === 0  && num % 5 === 0) {
+            allNumbers[index] = 'yu-gi-oh';
+        } else if(num % 2 === 0 && num % 3 === 0) {
+            allNumbers[index] = 'yu-gi'
+        } else if(num % 3 === 0 && num % 5 === 0) {
+            allNumbers[index] = 'gi-oh'
+        } else if(num % 2 === 0 && num % 5 === 0) {
+            allNumbers[index] = 'yu-oh'
+        } else if (num % 2) {
+            allNumbers[index] = 'yu';
         } else if(num % 3 === 0) {
             allNumbers[index] = 'gi';
         } else if(num % 5 === 0) {
             allNumbers[index] = 'oh'
-        }
-        
+        } 
     })
-    console.log(allNumbers)
+    return allNumbers;
 }
 
-checkNum(25)
+const view = checkNum(100)
+console.log(view)
